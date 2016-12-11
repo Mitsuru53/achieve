278d24a5636ab2276c421eb2167fcd622da0f08c
mitsuru_hashimoto <exhikkii@gmail.com>
2016-12-11 10:55:54 +0000

    first commit

---
+++ app/models/contact.rb
@@ -0,0 +1,6 @@
+class Contact < ActiveRecord::Base
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true
+    validates :email, presence: true,format:{with:VALID_EMAIL_REGEX}
+    validates :content, presence: true
+end
