63b2b055a12207608300ef910f59a1a6fae9a69b
mitsuru_hashimoto <exhikkii@gmail.com>
2016-12-18 03:30:22 +0000

    DIVE09

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# 100.times do |n|
-#   email = Faker::Internet.email
-#   password = "password"
-#     name = Faker::Name.name
-#   User.create!(email: email,password: password, password_confirmation: password, name: name)
-# end
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+    name = Faker::Name.name
+  User.create!(email: email,password: password, password_confirmation: password, name: name)
+end
 
 # 100.times do |n|
-n = 1
-while n <= 100
-  title = Faker::Name.title
-  content = Faker::StarWars.quote
-  user_id = n
-  Blog.create(title: title, content: content,user_id: user_id)
-  n += 1
-end
+# n = 1
+# while n <= 100
+#   title = Faker::Name.title
+#   content = Faker::StarWars.quote
+#   user_id = n
+#   Blog.create(title: title, content: content,user_id: user_id)
+#   n += 1
+# end
