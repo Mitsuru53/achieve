a381d75da54a4f2ba44c8fd25165e63eae42afc3
Mitsuru Hashimoto <exhikkii@gmail.com>
2016-12-19 19:34:57 +0900

    DIVE11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    addresses: "smtp.sendgrid.net",
+    address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
