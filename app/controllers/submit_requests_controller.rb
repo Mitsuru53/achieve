88adae137f6b5895cd23194f498fcb4456c435a5
Mitsuru Hashimoto <exhikkii@gmail.com>
2016-12-28 11:59:49 +0900

    DIVE18 secondtime

--- app/controllers/submit_requests_controller.rb
+++ app/controllers/submit_requests_controller.rb
@@ -1,6 +1,6 @@
 class SubmitRequestsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_submit_request, only: [:show, :edit, :update, :destroy]
+  before_action :set_submit_request, only: [:show, :edit, :update, :destroy, :approve, :reject]
 
   def index
     @submit_requests = SubmitRequest.where(request_user_id: current_user.id).where(status: 1)
@@ -55,21 +55,21 @@ class SubmitRequestsController < ApplicationController
 end
 
   def approve
-      if @submit_request.update(submit_request_params)
-        @submit_request.task.update(charge_id: current_user.id)
-        redirect_to inbox_submit_requests_path, notice: '依頼を承認しました。'
-      else
-        redirect_to inbox_submit_requests_path, notice: '不具合が発生しました、もう一度操作を行ってください。'
-      end
+    if @submit_request.update(submit_request_params)
+      @submit_request.task.update(charge_id: current_user.id)
+      redirect_to inbox_submit_requests_path, notice: '依頼を承認しました。'
+    else
+      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました、もう一度操作を行ってください。'
     end
+  end
 
-    def reject
-        if @submit_request.update(submit_request_params)
-          redirect_to inbox_submit_requests_path, notice: '依頼を却下しました。'
-        else
-          redirect_to inbox_submit_requests_path, notice: '不具合が発生しました、もう一度操作を行ってください。'
-        end
-      end
+  def reject
+    if @submit_request.update(submit_request_params)
+       redirect_to inbox_submit_requests_path, notice: '依頼を却下しました。'
+    else
+      redirect_to inbox_submit_requests_path, notice: '不具合が発生しました、もう一度操作を行ってください。'
+    end
+  end
 
   private
     def set_submit_request
